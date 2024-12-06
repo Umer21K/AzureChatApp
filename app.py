45a9e371f45417af73d1de0fc55010feafa3886d
Abid Mustafa <abidmustafa15@gmail.com>
2024-12-06 07:09:49 +0500

    Update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from blob_storage import *
 from database import *
 # from blob_storage import *
+from dotenv import load_dotenv
 import os
 
 app = Flask(__name__, static_folder='static', template_folder='static')
